ef3e3e1f7b7d0c223e9d2035266896f1e7b36650
zachary <groff.zackm@gmail.com>
2023-01-23 16:59:19 -0500

    add docstrings

--- py/lib/kodawari.models/models/user.py
+++ py/lib/kodawari.models/models/user.py
@@ -10,23 +10,32 @@ def display_name_must_be_alphanumeric_or_underscore(display_name: str) -> str:
 _display_name_field = Field(
     min_length=4,
     max_length=12,
-    description="The display name of a user, not required to be unique",
+    description="The non-unique alphanumeric display name of a user, restricted to a minimum of four and a maximum of twelve characters",
 )
 _description_field = Field(
     max_length=1000,
     default="",
-    description="The user provided description for their profile",
+    description="The user provided description for their profile, restricted to a maximum of 1000 characters, defaulting to empty string",
 )
 
 
 class UserSchema(BaseModel):
+    """A user's profile information.
+
+    Attributes:
+        id: The unique identifier for a user
+        display_name: The non-unique alphanumeric display name of a user, restricted to a minimum of 4 and a maximum of 12 characters
+        description: The user provided description for their profile, restricted to a maximum of 1000 characters, defaulting to empty string
+        joined: The date a user was created, expressed as a unix timestamp in milliseconds
+    """
+
     id: int = Field(gt=0, description="The unique identifier for a user")
     display_name: str = _display_name_field
     description: str = _description_field
     joined: int = Field(
         gt=1674484829053,
         lt=9999999999999,
-        description="The date a user was created, expressed as a unix timestamp",
+        description="The date a user was created, expressed as a unix timestamp in milliseconds",
     )
 
     _validate_display_name = validator("display_name", allow_reuse=True)(
@@ -35,6 +44,13 @@ class UserSchema(BaseModel):
 
 
 class UserCreateRequest(BaseModel):
+    """The required request body for creating a UserSchema object
+
+    Attributes:
+        display_name: The non-unique alphanumeric display name of a user, restricted to a minimum of 4 and a maximum of 12 characters
+        description: The user provided description for their profile, restricted to a maximum of 1000 characters, defaulting to empty string
+    """
+
     display_name: str = _display_name_field
     description: str = _description_field
 
@@ -44,6 +60,13 @@ class UserCreateRequest(BaseModel):
 
 
 class UserPatchRequest(BaseModel):
+    """The required request body for patching a UserSchema object
+
+    Attributes:
+        display_name: The (optional) non-unique alphanumeric display name of a user, restricted to a minimum of 4 and a maximum of 12 characters
+        description: The (optional) user provided description for their profile, restricted to a maximum of 1000 characters, defaulting to empty string
+    """
+
     display_name: str | None = _display_name_field
     description: str | None = _description_field
 
--- py/projects/user_api/user_api/main.py
+++ py/projects/user_api/user_api/main.py
@@ -24,6 +24,13 @@ current_id: int = 0
 
 
 def get_id() -> int:
+    """A temporary function for generating a unique ID within the context of the API's lifetime
+
+    This method will eventually be deprecated in favour of a Snowflake ID generator.
+
+    Returns:
+        A unique ID within the context of the API's lifetime
+    """
     global current_id
     current_id += 1
     return current_id
@@ -33,6 +40,15 @@ session: Session | None = None
 
 
 async def cassandra_session() -> Session:
+    """Retrieves an acsylla Session object, for accessing Cassandra.
+
+    Retrieves an acsylla Session object after connecting to a cassandra cluster specified by the environment variable CASSANDRA_CLUSTER_NAME. If a Session object has already been created during the API's lifetime, it is returned without a new Session object being created.
+
+    Returns:
+        An acsylla.Session object, for accessing Cassandra.
+    Raises:
+        Exception: CASSANDRA_CLUSTER_NAME is not set.
+    """
     global session
     if session is not None:
         return session
@@ -54,11 +70,26 @@ async def cassandra_session() -> Session:
 
 @app.get("/health")
 async def health() -> str:
+    """Retrieves the health status of the API.
+
+    Returns:
+        A string of "healthy" if the API is healthy.
+    """
     return "healthy"
 
 
 @app.get("/user/{id}", status_code=status.HTTP_200_OK)
 async def get(id: int, session: Session = Depends(cassandra_session)) -> UserSchema:
+    """Retrieves a user.
+
+    Args:
+        id: The identifier for the requested User resource.
+        session: The Session required to access User data in Cassandra.
+    Returns:
+        A UserSchema, containing the requested User data.
+    Raises:
+        HTTPException: The requested User resource could not be retrieved.
+    """
     statement: Statement = create_statement(
         "SELECT * FROM user WHERE id=?", parameters=1
     )
@@ -86,6 +117,16 @@ async def post(
     user_create_request: UserCreateRequest,
     session: Session = Depends(cassandra_session),
 ) -> None:
+    """Creates a user.
+
+    Patches a user and sets the Location header to the created resource path, using properties defined in the user_create_request, a generated id, and a unix timestamp.
+
+    Args:
+        response: The FastAPI response class used for setting the Location header of the created resource path.
+        user_create_request: The UserSchema properties to set on the created resource.
+        session: The Session required to access User data in Cassandra.
+    """
+
     statement: Statement = create_statement(
         "INSERT INTO user (id, display_name, description, joined) VALUES (?, ?, ?, toTimestamp(now()))",
         parameters=3,
@@ -107,6 +148,16 @@ async def post(
 def get_patch_statement(
     user_patch_request: UserPatchRequest, bearer_claims: BearerClaims
 ) -> Statement | None:
+    """Retrieves a Statement for patching.
+
+    Retrieves a Statement for patching a User resource in Cassandra.
+
+    Args:
+        user_patch_request: The User resource properties to update.
+        bearer_claims: The claims included on the Bearer token in the request.
+    Returns:
+        An optional Statement for patching a User resource. None if no data requires patching.
+    """
     statement: Statement
     arguments: list[Any]
 
@@ -165,6 +216,16 @@ async def patch(
     bearer_claims: BearerClaims = Depends(authenticate),
     session: Session = Depends(cassandra_session),
 ) -> None:
+    """Patches a user.
+
+    Patches a user and sets the Location header to the modified resource path, using the ID retrieved from bearer_claims and properties defined in the user_patch_request.
+
+    Args:
+        response: The FastAPI response class used for setting the Location header of the modified resource path.
+        user_patch_request: The User resoruce properties to update.
+        bearer_claims: The claims included on the Bearer token in the request.
+        session: The Session required to access User data in Cassandra.
+    """
     statement: Statement | None = get_patch_statement(user_patch_request, bearer_claims)
     if statement is not None:
         await session.execute(statement)
@@ -177,6 +238,14 @@ async def delete(
     bearer_claims: BearerClaims = Depends(authenticate),
     session: Session = Depends(cassandra_session),
 ) -> None:
+    """Deletes a user.
+
+    Deletes a User resource using the ID retrieved from bearer_claims.
+
+    Args:
+        bearer_claims: The claims included on the Bearer token in the request.
+        session: The Session required to access User data in Cassandra.
+    """
     statement: Statement = create_statement(
         "DELETE FROM user WHERE id=? IF EXISTS", parameters=1
     )
